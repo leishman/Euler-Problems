9a2cfb08bb36d2dc805054a1430423454cce0759
Alexander Leishman <leishman3@gmail.com>
2013-06-23 16:02:42 -0400

    created shell for prob 248

---
+++ Euler248.rb
@@ -0,0 +1,11 @@
+# Solution to Euler Problem 248
+# By Alexander Leishman June 23, 2013
+# Define Euler Totient Function to calculate the number of relatively prime numbers below n
+
+def phi(int)	#Totient function phi(n)
+
+# TO BE COMPLETED LATER
+
+
+end
+
